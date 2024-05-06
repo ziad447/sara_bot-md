eeac52705f5430aef98212d4bd54c5762aa93969
omar-lite <158737422+omar-lite@users.noreply.github.com>
2024-05-06 19:13:12 +0300

    Update صراحه.js

--- plugins/صراحه.js
+++ plugins/صراحه.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*♚  مرحبا بك في قسم لعبة صراحه ♚*\n          ꔹ━━━━━━━━━━━ꔹ\n*『${pickRandom(global.bjssvjs)}』*\n*ꔹ━━━ꔹ❰ لوفي-بوت ❱ꔹ━━━ꔹ*`, m)
+conn.reply(m.chat,`*♚  مرحبا بك في قسم لعبة صراحه ♚*\n          ꔹ━━━━━━━━━━━ꔹ\n*『${pickRandom(global.bjssvjs)}』*\n*ꔹ━━━ꔹ❰ لايت-بوت ❱ꔹ━━━ꔹ*`, m)
 }
 handler.help = ['uwvssj']
 handler.tags = ['fun']
