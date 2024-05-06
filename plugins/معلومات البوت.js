6430bc2ede7840f2df4f7fe32f2a76b60b7749a3
omar-lite <158737422+omar-lite@users.noreply.github.com>
2024-05-06 03:34:57 +0300

    Create معلومات البوت.js

---
+++ plugins/معلومات البوت.js
@@ -0,0 +1,123 @@
+const _0x4f3522 = _0x2d74;
+(function (_0x49a7ff, _0x3fcdca) {
+    const _0x2456c8 = _0x2d74;
+    const _0x14f1d9 = _0x49a7ff();
+    while (!![]) {
+        try {
+            const _0x3a2871 = -parseInt(_0x2456c8(0x6)) / 0x1 + parseInt(_0x2456c8(0x27)) / 0x2 + parseInt(_0x2456c8(0x1a)) / 0x3 + parseInt(_0x2456c8(0x7)) / 0x4 + parseInt(_0x2456c8(0x21)) / 0x5 + -parseInt(_0x2456c8(0x2e)) / 0x6 + -parseInt(_0x2456c8(0x38)) / 0x7;
+            if (_0x3a2871 === _0x3fcdca) {
+                break;
+            } else {
+                _0x14f1d9['push'](_0x14f1d9['shift']());
+            }
+        } catch (_0x1e1890) {
+            _0x14f1d9['push'](_0x14f1d9['shift']());
+        }
+    }
+}(_0xd63f, 0x2358f));
+
+function _0x2d74(_0x808170, _0xd63f94) {
+    const _0x2d7464 = _0xd63f();
+    _0x2d74 = function (_0x732fae, _0x3ecf17) {
+        _0x732fae = _0x732fae - 0x0;
+        let _0x33a2eb = _0x2d7464[_0x732fae];
+        return _0x33a2eb;
+    };
+    return _0x2d74(_0x808170, _0xd63f94);
+}
+import {
+    generateWAMessageFromContent
+} from '@whiskeysockets/baileys';
+import _0x3fe54b from 'os';
+import _0x4c12d9 from 'util';
+import _0x49a600 from 'human-readable';
+import _0x13c10a from '@whiskeysockets/baileys';
+import _0x341039 from 'fs';
+import {
+    performance
+} from 'perf_hooks';
+const handler = async (_0x6e8a7e, {
+    conn: _0x1840f0,
+    usedPrefix: _0x59bf41
+}) => {
+    const _0x44f6bb = _0x2d74;
+    const _0x2fb525 = process['uptime']() * 0x3e8;
+    const _0x5dd37c = clockString(_0x2fb525);
+    const _0xed4701 = Object['keys'](global['db'][_0x44f6bb(0x29)][_0x44f6bb(0x2a)])['length'];
+    const _0x74a8bf = Object[_0x44f6bb(0x1b)](_0x1840f0['chats'])[_0x44f6bb(0x2)](([_0x333bce, _0x146948]) => _0x333bce && _0x146948[_0x44f6bb(0x22)]);
+    const _0x3e51f3 = _0x74a8bf[_0x44f6bb(0x2)](([_0x1660c8]) => _0x1660c8[_0x44f6bb(0x5)](_0x44f6bb(0x32)));
+    const _0x480f13 = _0x74a8bf[_0x44f6bb(0x2)](([_0x2012a6]) => _0x2012a6[_0x44f6bb(0x5)](_0x44f6bb(0x32)));
+    const _0x4d3ea1 = process[_0x44f6bb(0x1c)]();
+    const {
+        restrict: _0x287bc4,
+        antiCall: _0x5d89ec,
+        antiprivado: _0x4c926e,
+        modejadibot: _0x2971a4
+    } = global['db'][_0x44f6bb(0x29)][_0x44f6bb(0x31)][_0x1840f0[_0x44f6bb(0x17)][_0x44f6bb(0xb)]] || {};
+    const {
+        autoread: _0x469be0,
+        gconly: _0x11c2ac,
+        pconly: _0x5e5595,
+        self: _0x8e3052
+    } = global['opts'] || {};
+    const _0x1455e7 = performance[_0x44f6bb(0x4)]();
+    const _0x430c23 = performance[_0x44f6bb(0x4)]();
+    const _0xf05c38 = _0x430c23 - _0x1455e7;
+    const _0x4d8156 = (_0x44f6bb(0x12) + _0x59bf41 + _0x44f6bb(0x36) + (_0x74a8bf[_0x44f6bb(0x1f)] - _0x480f13[_0x44f6bb(0x1f)]) + _0x44f6bb(0x25) + _0x480f13['length'] + _0x44f6bb(0x0) + _0x74a8bf[_0x44f6bb(0x1f)] + _0x44f6bb(0x8) + _0x5dd37c + _0x44f6bb(0x15) + _0xed4701 + ' رقم*\x0a╠➥ [☑️] الصحين: ' + (_0x469be0 ? _0x44f6bb(0x1e) : _0x44f6bb(0x9)) + _0x44f6bb(0x24) + (_0x287bc4 ? _0x44f6bb(0x1e) : _0x44f6bb(0x9)) + _0x44f6bb(0x2d) + (_0x5e5595 ? '*مفعل*' : '*غير مفعل*') + _0x44f6bb(0x34) + (_0x11c2ac ? _0x44f6bb(0x1e) : _0x44f6bb(0x9)) + '\x0a╠➥ [🌎] مود: ' + (_0x8e3052 ? _0x44f6bb(0x39) : _0x44f6bb(0x23)) + _0x44f6bb(0x3) + (_0x4c926e ? _0x44f6bb(0x1e) : _0x44f6bb(0x9)) + '\x0a╠➥ [🤖] البوتات الفرعيه: ' + (_0x2971a4 ? '*مفعل*' : _0x44f6bb(0x9)) + _0x44f6bb(0x18) + (_0x5d89ec ? _0x44f6bb(0x1e) : _0x44f6bb(0x9)) + _0x44f6bb(0x14) + _0xf05c38 + _0x44f6bb(0x30))[_0x44f6bb(0x33)]();
+    const _0x54dbca = ['pdf', _0x44f6bb(0x2b), _0x44f6bb(0xf), _0x44f6bb(0x10), _0x44f6bb(0x2f)];
+    const _0x5510c1 = _0x54dbca[Math['floor'](Math[_0x44f6bb(0xa)]() * _0x54dbca[_0x44f6bb(0x1f)])];
+    const _0xd6561f = {
+        'document': {
+            'url': _0x44f6bb(0x20)
+        },
+        'mimetype': _0x44f6bb(0x11) + _0x5510c1,
+        'fileName': _0x44f6bb(0xd),
+        'fileLength': 0x5af3107a3fff,
+        'pageCount': 0xc8,
+        'contextInfo': {
+            'forwardingScore': 0xc8,
+            'isForwarded': !![],
+            'externalAdReply': {
+                'mediaUrl': _0x44f6bb(0x20),
+                'mediaType': 0x2,
+                'previewType': _0x44f6bb(0x16),
+                'title': _0x44f6bb(0x28),
+                'body': wm,
+                'thumbnail': imagen1,
+                'sourceUrl': _0x44f6bb(0x20)
+            }
+        },
+        'caption': _0x4d8156,
+        'footer': wm,
+        'headerType': 0x6
+    };
+    _0x1840f0[_0x44f6bb(0x37)](_0x6e8a7e[_0x44f6bb(0x1d)], _0xd6561f, {
+        'quoted': _0x6e8a7e
+    });
+};
+handler[_0x4f3522(0x35)] = ['infobot', 'speed'];
+handler[_0x4f3522(0x1)] = [_0x4f3522(0x26), 'tools'];
+handler[_0x4f3522(0x2c)] = /^(ping|speed|معلومات)$/i;
+
+function _0xd63f() {
+    const _0x13198c = ['vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/', '\x0a╠═〘 _معلومات البوت_ 〙 ═\x0a╠\x0a╠➥ [🤴🏻] المصممين: *⁨𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩*\x0a╠➥ [#️⃣] الارقام: *+201159106301* و *+201280244814*\x0a╠➥ [🎳] البادئه;: *', 'floor', '\x0a╠➥ [👨‍🦯] السرعه: \x0a╠  *', '*\x0a╠➥ [🎩] المستخدمين: *', 'pdf', 'user', '\x0a╠➥ [📵] مضاد المكالمات: ', 'toString', '250011WlPnkk', 'entries', 'memoryUsage', 'chat', '*مفعل*', 'length', 'https://chat.whatsapp.com/H93YpO0LkJNHoTmtIhcL1z', '1403015HVUDKR', 'isChats', '*عام*', '\x0a╠➥ [❗] التقيد: ', '*\x0a╠➥ [🦜] الجروبات: *', 'info', '539328kiyuoI', 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ', 'data', 'users', 'zip', 'command', ' \x0a╠➥ [💬] خاص فقط: ', '36696ziHOau', 'vnd.openxmlformats-officedocument.wordprocessingml.document', ' ms* \x0a╠\x0a╠═〘 ⁨𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩ 〙 ═\x0a', 'settings', '@g.us', 'trim', '\x0a╠➥ [🏢] جروبات فقط: ', 'help', '*\x0a╠➥ [🔐] شات الخاص: *', 'sendMessage', '3466281EgUznn', '*خاص*', 'map', '* \x0a╠➥ [💡] عدد المحادثات: *', 'tags', 'filter', '\x0a╠➥ [💬] مضاد الخاص: ', 'now', 'endsWith', '159788KEWCNi', '689064HQaqIv', '* \x0a╠➥ [🚀] النشاط: *', '*غير مفعل*', 'random', 'jid', 'join', '「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」', 'padStart'];
+    _0xd63f = function () {
+        return _0x13198c;
+    };
+    return _0xd63f();
+}
+export default handler;
+
+function clockString(_0x5570db) {
+    const _0x2b7348 = _0x2d74;
+    const _0x185f16 = Math[_0x2b7348(0x13)](_0x5570db / 0x36ee80);
+    const _0x450716 = Math[_0x2b7348(0x13)](_0x5570db / 0xea60) % 0x3c;
+    const _0x20d58a = Math['floor'](_0x5570db / 0x3e8) % 0x3c;
+    console['log']({
+        'ms': _0x5570db,
+        'h': _0x185f16,
+        'm': _0x450716,
+        's': _0x20d58a
+    });
+    return [_0x185f16, _0x450716, _0x20d58a][_0x2b7348(0x3a)](_0x2a9b1d => _0x2a9b1d[_0x2b7348(0x19)]()[_0x2b7348(0xe)](0x2, 0x0))[_0x2b7348(0xc)](':');
+}
