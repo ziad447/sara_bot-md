61b6c4402f2177691d78581b720915dcb657d0d4
omar-lite <158737422+omar-lite@users.noreply.github.com>
2024-05-07 14:23:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = "*『👑𝚃𝙷𝙴_𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑』*"
+global.botnumber = "*『👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑』*"
 global.confirmCode = ""
 
 global.owner = [['201280244814', '⁨𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩', true],
@@ -16,12 +16,12 @@ global.owner = [['201280244814', '⁨𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻�
 global.suittag = ['201280244814'];
 global.prems = ['201280244814'];
 
-global.packname = '☾︎ ⁨𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩ ☽︎';
-global.author = '👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑';
-global.wm = '👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑';
-global.titulowm = '☾︎ 👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑 ☽︎';
-global.titulowm2 = `☾︎ 👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑 ☽︎`
-global.igfg = '☾︎ 👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑 ☽︎';
+global.packname = '𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩';
+global.author = '👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑';
+global.wm = '👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑';
+global.titulowm = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
+global.titulowm2 = `☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎`
+global.igfg = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
 global.wait = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -42,9 +42,9 @@ global.mes = d.toLocaleDateString('ar', {month: 'long'});
 global.año = d.toLocaleDateString('ar', {year: 'numeric'});
 global.tiempo = d.toLocaleString('ar-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\n☾︎ 👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑 ☽︎`;
-global.gt = '☾︎ 👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑 ☽︎';
-global.mysticbot = '☾︎ 👑𝙻𝙸𝙶𝙷𝚃_𝙱𝙾𝚃👑 ☽︎';
+global.wm2 = `${dia} ${fecha}\n☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎`;
+global.gt = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
+global.mysticbot = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
 global.md = 'https://chat.whatsapp.com/H93YpO0LkJNHoTmtIhcL1z';
 global.mysticbot = 'atom.bio/harley_light';
 global.waitt = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
